593ead8d0698fc7a8cb5d236309835635ead47a2
yury-ff <yury.r@forkedfinance.xyz>
2023-10-28 19:13:41 +0100

    first ui changes

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="p-3 bg-black text-white">
+      <div className="container mx-auto">
+        <p>&copy; 2023 Promelo. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import Image from "next/image";
+import logo from "../../public/assets/logo.png";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex grid-cols-2 flex-grow p-4">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold">DeFi Credit Score</h1>
+          <p className="text-2xl">Undercollateralized Borrowing.</p>
+        </div>
+        <div>
+          <Image
+            // src={logo}
+            width={900}
+            height={550}
+            alt="logo"
+            // className="w-32 h-32 rounded-full mx-auto"
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
